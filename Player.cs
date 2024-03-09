4e85d72981b69cb8e7c1496aa031c993954aec06
Oleh Ostrovskyi <ostroleg0032@gmail.com>
2024-03-10 01:12:19 +0200

    fix movement

--- Player.cs
+++ Player.cs
@@ -4,57 +4,56 @@ using Godot.Collections;
 
 public partial class Player : Node3D
 {
-	private bool _moving;
+    private bool _moving;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        var movementSpeed = TimeSpan.FromMilliseconds(300);
 
-	public override void _Input(InputEvent @event)
-	{
-		var movementSpeed = TimeSpan.FromMilliseconds(100);
-		if (@event.IsAction("forward") && !_moving)
-		{
-			_moving = true;
-			var tween = CreateTween();
-			tween.TweenProperty(this, "position", Position + Vector3.Forward, movementSpeed.TotalSeconds);
-			tween.TweenCallback(Callable.From(() => _moving = false));
-			tween.Play();
-		}
-		
-		if (@event.IsAction("backward") && !_moving)
-		{
-			_moving = true;
-			var tween = CreateTween();
-			tween.TweenProperty(this, "position", Position + Vector3.Back, movementSpeed.TotalSeconds);
-			tween.TweenCallback(Callable.From(() => _moving = false));
-			tween.Play();
-		}
-		
-		if (@event.IsAction("left") && !_moving)
-		{
-			_moving = true;
-			var tween = CreateTween();
-			tween.TweenProperty(this, "position", Position + Vector3.Left, movementSpeed.TotalSeconds);
-			tween.TweenCallback(Callable.From(() => _moving = false));
-			tween.Play();
-		}
-		
-		if (@event.IsAction("right") && !_moving)
-		{
-			_moving = true;
-			var tween = CreateTween();
-			tween.TweenProperty(this, "position", Position + Vector3.Right, movementSpeed.TotalSeconds);
-			tween.TweenCallback(Callable.From(() => _moving = false));
-			tween.Play();
-		}
-		
-		base._Input(@event);
-	}
+        if (Input.IsActionPressed("forward") && !_moving)
+        {
+            _moving = true;
+            var tween = CreateTween();
+            tween.TweenProperty(this, "position", Position + Vector3.Forward, movementSpeed.TotalSeconds);
+            tween.TweenCallback(Callable.From(() => _moving = false));
+            tween.Play();
+        }
+
+        if (Input.IsActionPressed("backward") && !_moving)
+        {
+            _moving = true;
+            var tween = CreateTween();
+            tween.TweenProperty(this, "position", Position + Vector3.Back, movementSpeed.TotalSeconds);
+            tween.TweenCallback(Callable.From(() => _moving = false));
+            tween.Play();
+        }
+
+        if (Input.IsActionPressed("left") && !_moving)
+        {
+            _moving = true;
+            var tween = CreateTween();
+            tween.TweenProperty(this, "position", Position + Vector3.Left, movementSpeed.TotalSeconds);
+            tween.TweenCallback(Callable.From(() => _moving = false));
+            tween.Play();
+        }
+
+        if (Input.IsActionPressed("right") && !_moving)
+        {
+            _moving = true;
+            var tween = CreateTween();
+            tween.TweenProperty(this, "position", Position + Vector3.Right, movementSpeed.TotalSeconds)
+                .SetEase(Tween.EaseType.In)
+                .SetTrans(Tween.TransitionType.Sine);
+            
+            tween.TweenCallback(Callable.From(() => _moving = false));
+            tween.Play();
+        }
+
+    }
 }
